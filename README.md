76384e403317f9a57ed734f62927f9397ed8a54a
kchristin <christingeorge321@gmail.com>
2021-01-08 10:11:35 -0500

    added funtion
    added sys and random

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Figuring_out_Python
+
+## Automating boring stuff with python
---
+++ funtion.py
@@ -0,0 +1,10 @@
+import random, sys
+#random funtion gives you a random value
+print(random.randint(1,10))
+# you could also use 'from random import *' and 'randint(1, 10)' for the same result
+
+#sys funtion
+print('Hello world')
+sys.exit()
+#sys.exit() terimate the funtion at this point.
+print('Goodbye world')

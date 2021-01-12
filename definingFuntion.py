19f239b9a428ac9d0092e68b65dc37f8853dbe11
kchristin <christingeorge321@gmail.com>
2021-01-12 11:56:42 -0500

    added string concantination

---
+++ definingFuntion.py
@@ -0,0 +1,10 @@
+def hello():
+    print('hello')
+    print('howdy')
+
+hello()
+hello()
+
+def hello2(name):
+    print("The name is " + name + " and has " + str(len(name)) + 'letters')
+    # the str is for string concantination

3898b90778d8e000902aa1104b501a5dded6a5ee
kchristin <christingeorge321@gmail.com>
2021-01-12 12:12:08 -0500

    tricks of print

--- definingFuntion.py
+++ definingFuntion.py
@@ -7,4 +7,11 @@ hello()
 
 def hello2(name):
     print("The name is " + name + " and has " + str(len(name)) + 'letters')
-    # the str is for string concantination
+    # the str is for string concantination(can't + to string without str)
+
+#trick for print
+print('Hello', end='')# dosen't add the endline for the print
+print('World') 
+
+print('stuff', 'stuff', 'stuff')
+print('stuff', 'stuff', 'stuff', sep='ABC')#instead of spaces, it is ABC
